0e0d33468109bd2e951925937b80a45f012a6961
Kai Röder <roeder.kai@gmail.com>
2021-05-01 11:47:22 +0200

    feat(ng-add): add ng add schematic

--- src/ng-add/index.spec.ts
+++ src/ng-add/index.spec.ts
@@ -1,16 +1,34 @@
-import {Tree} from '@angular-devkit/schematics';
-import {SchematicTestRunner} from '@angular-devkit/schematics/testing';
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
-
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
+describe('ng-add', () => {
+    it('should add devDependencies', async () => {
+        const angularJson = {
+            dependencies: {},
+            devDependencies: {},
+        };
 
-describe('ng-schematics-toolkit', () => {
-    it('works', async () => {
+        const angularJsonExpected = {
+            dependencies: {},
+            devDependencies: {
+                '@angular-devkit/architect': '~0.1102.0',
+                '@angular-devkit/core': '~11.2.11',
+                '@angular-devkit/schematics': '~11.2.11',
+                '@schematics/angular': '~11.2.11',
+            },
+        };
+
         const runner = new SchematicTestRunner('schematics', collectionPath);
-        const tree = await runner.runSchematicAsync('ng-add', {}, Tree.empty()).toPromise();
+        const tree = Tree.empty();
+        tree.create('package.json', JSON.stringify(angularJson));
 
-        expect(tree.files).toEqual([]);
+        await runner.runSchematicAsync('ng-add', {}, tree).toPromise();
+
+        expect(JSON.parse(tree.read('package.json')!.toString())).toEqual(
+            angularJsonExpected
+        );
     });
 });
--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,10 +1,38 @@
-import {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+    addPackageJsonDependency,
+    NodeDependencyType,
+} from '@schematics/angular/utility/dependencies';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function ngAdd(_options: any): Rule {
-    return (tree: Tree, _context: SchematicContext) => {
-        return tree;
+    return (host: Tree, _context: SchematicContext) => {
+        addDependencies(host);
+
+        return host;
     };
 }
+
+// const getCurrentInstalledAngularVersion = (host: Tree) => {
+//     const angularCore = getPackageJsonDependency(host, '@angular/core');
+//     if (!angularCore) {
+//         throw new SchematicsException(`Could not find @angular/core in package.json dependencies`);
+//     }
+//     return angularCore.version;
+// }
+
+const addDependencies = (host: Tree) => {
+    const devDependencies: { [key: string]: string } = {
+        '@angular-devkit/architect': '~0.1102.0',
+        '@angular-devkit/core': '~11.2.11',
+        '@angular-devkit/schematics': '~11.2.11',
+        '@schematics/angular': '~11.2.11',
+    };
+
+    for (const devDependenciesKey in devDependencies) {
+        addPackageJsonDependency(host, {
+            type: NodeDependencyType.Dev,
+            name: devDependenciesKey,
+            version: devDependencies[devDependenciesKey],
+        });
+    }
+};
