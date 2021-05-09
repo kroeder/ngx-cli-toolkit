8e6a51c06c5480220750aa975772f31bae3b3470
Kai Roeder <roeder.kai@gmail.com>
2021-05-09 19:20:54 +0200

    feat(builders): add noop builder
    This is a first working noop builder that will
    be used for building a schematics project

---
+++ src/builders/build-schematics/index.ts
@@ -0,0 +1,20 @@
+import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
+import { Observable, of } from 'rxjs';
+import { json } from '@angular-devkit/core';
+
+interface SchematicsBuilderOptions {
+}
+
+export function buildSchematics(
+    _options: SchematicsBuilderOptions,
+    { logger }: BuilderContext
+): Observable<BuilderOutput> {
+    logger.info('Yay!');
+    return of({
+        success: true
+    });
+}
+
+export default createBuilder<json.JsonObject & SchematicsBuilderOptions>(
+    buildSchematics
+);
