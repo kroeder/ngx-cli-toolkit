4bdb86bf5c0f0231a57ed18651d5a4fc0a59dde6
Kai Roeder <roeder.kai@gmail.com>
2021-05-20 12:10:18 +0200

    refactor tests

--- src/ng-generate/init/index.spec.ts
+++ src/ng-generate/init/index.spec.ts
@@ -61,10 +61,47 @@ describe('ng-generate', () => {
         }
     };
 
-    it('should initialize a schematics project', async () => {
-        const runner = new SchematicTestRunner('schematics', collectionPath);
-        const tree = Tree.empty();
+    const packageJson = {
+        'name': 'ui',
+        'version': '0.0.1',
+        'peerDependencies': {
+            '@angular/common': '^11.2.13',
+            '@angular/core': '^11.2.13'
+        },
+        'dependencies': {
+            'tslib': '^2.0.0'
+        }
+    };
+
+    const packageJsonExpected = {
+        'name': 'ui',
+        'version': '0.0.1',
+        'schematics': './schematics/collection.json',
+        'ng-update': {
+            'migrations': './schematics/migrations.json',
+            'packageGroup': []
+        },
+        'peerDependencies': {
+            '@angular/common': '^11.2.13',
+            '@angular/core': '^11.2.13'
+        },
+        'dependencies': {
+            'tslib': '^2.0.0'
+        }
+    };
+
+    let tree: Tree;
+    let runner: SchematicTestRunner;
+    const schematicsDirectory = path.join(
+        angularJson.projects.ui.root,
+        'schematics'
+    );
+
+    beforeEach(async () => {
+        runner = new SchematicTestRunner('schematics', collectionPath);
+        tree = Tree.empty();
         tree.create('angular.json', JSON.stringify(angularJson));
+        tree.create('package.json', JSON.stringify(packageJson));
 
         await runner
             .runSchematicAsync(
@@ -74,18 +111,30 @@ describe('ng-generate', () => {
             )
             .toPromise();
 
-        const schematicsDirectory = path.join(
-            angularJson.projects.ui.root,
-            'schematics'
-        );
+
+    });
+
+    it('should create migrations.json', () => {
         expect(
             tree.exists(path.join(schematicsDirectory, 'migrations.json'))
         ).toBe(true);
+    });
+
+    it('should create collection.json', () => {
         expect(
             tree.exists(path.join(schematicsDirectory, 'collection.json'))
         ).toBe(true);
+    });
+
+    it('should add `build-schematics` builder to angukar.json', () => {
         expect(
             JSON.parse(tree.read('angular.json')?.toString('utf8')!)
         ).toStrictEqual(angularJsonExpected);
+    });
+
+    it('should add schematics entries in package.json', () => {
+        expect(
+            JSON.parse(tree.read('package.json')?.toString('utf8')!)
+        ).toStrictEqual(packageJsonExpected);
     });
 });
